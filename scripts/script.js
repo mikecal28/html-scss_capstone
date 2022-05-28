a3d470951d57478f0e1182d75a868140f2aea0fa
mikecal28 <mt28.caldwell@gmail.com>
2022-05-27 18:21:17 -0600

    "finished up navbar-wrapper and hero-wrapper and added background to body tag"

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
+const debounce = (fn) => {
+  let frame;
+
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
 };
 
-document.addEventListener("scroll", storeScroll);
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
 
 storeScroll();
